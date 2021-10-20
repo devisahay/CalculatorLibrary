99c6ee6c9518e76cfed294caa613dd4796d89c37
Devisahay Mishra <devisahay.mishra@learningmatemumbai.local>
2021-10-20 16:14:09 +0530

    test with ci

--- helpers/init_git_repo.sh
+++ helpers/init_git_repo.sh
@@ -23,19 +23,19 @@
 git init
 git config user.email "devisahay.mishra@learningmate.com"
 git config user.name "Devisahay Mishra"
-echo ${BASE_REPO_URL}
-git commit -m "empty commit again"
+
+git commit -m "empty commit"
 git remote add origin "${BASE_REPO_URL}"
 git fetch origin master
 
-# # Fetch all PRs to get history for PRs created from forked repos
-# git fetch origin +refs/pull/*/merge:refs/remotes/origin/pr/*
+# Fetch all PRs to get history for PRs created from forked repos
+git fetch origin +refs/pull/*/merge:refs/remotes/origin/pr/*
 
-# git reset --hard "origin/pr/${PR_NUMBER}"
+git reset --hard "origin/pr/${PR_NUMBER}"
 
-# if ! git rebase "origin/${BASE_BRANCH}"
-# then
-#   exit 1
-# fi
+if ! git rebase "origin/${BASE_BRANCH}"
+then
+  exit 1
+fi
 
 echo "successfully rebased PR  #${PR_NUMBER} on master"
